def35f08d3e42205427d8e331d685bf758e7b2c9
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-01-01 03:22:40 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import shortid from 'shortid'; // Імпорт shortid
 
 class App extends Component {
   state = {
